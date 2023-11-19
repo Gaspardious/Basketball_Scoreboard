16792d0beb0523fa9c169a7ca86949492a63fd5e
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2023-11-19 09:12:39 +0100

    Almost done

---
+++ index.js
@@ -0,0 +1,36 @@
+let count1 = document.getElementById("count1")
+let count2 = document.getElementById("count2")
+let homecount = 0
+let guestcount = 0
+
+function increment () {
+    homecount += 1
+    count1.textContent = homecount
+}
+
+
+function increment2 () {
+    homecount += 2
+    count1.textContent = homecount
+}
+
+
+function increment3 () {
+    homecount += 3
+    count1.textContent = homecount
+}
+
+function increment4 () {
+    guestcount += 1
+    count2.textContent = guestcount
+}
+
+function increment5 () {
+    guestcount += 2
+    count2.textContent = guestcount
+}
+
+function increment6 () {
+    guestcount += 3
+    count2.textContent = guestcount
+}
